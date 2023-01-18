74f661172ff0f1e0a18e0f94d59712944813bcb8
Slava <kasterov.v@gmail.com>
2023-01-18 17:24:06 +0200

    Repository pattern implementation

---
+++ DLL/Repository/AssignmentRepository.cs
@@ -0,0 +1,66 @@
+﻿
+using DAL.Data;
+using DAL.Entities;
+using DAL.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repository;
+
+public class AssignmentRepository : IAssignmentRepository
+{
+    private readonly TaskerDbContext _DbContext;
+
+    public AssignmentRepository(TaskerDbContext dbContext)
+    {
+        _DbContext = dbContext;
+    }
+
+    public async Task DeleteAssignment(int id)
+    {
+        var assignment = await GetAssignmentById(id);
+
+        _DbContext.Assignments.Remove(assignment);
+        await _DbContext.SaveChangesAsync();
+    }
+
+    public async Task<Assignment?> GetAssignmentById(int id)
+    {
+        var assignment = await _DbContext.Assignments
+            .FirstOrDefaultAsync(x => x.Id == id);
+
+        return assignment is not null ? assignment : null; 
+    }
+
+    public async Task<IEnumerable<Assignment>> GetAssignmentsByStatus(EnumStatus status)
+    {
+        var assignments = await _DbContext.Assignments
+            .Where(x => x.Status == status)
+            .Select(x => new Assignment
+            {
+                Id = x.Id,
+                Status = x.Status,
+                Description = x.Description,
+                Name = x.Name
+            })
+            .ToListAsync();
+
+        return assignments;
+    }
+
+    public async Task PutAssignment(Assignment assignment)
+    {
+        await _DbContext.Assignments.AddAsync(assignment);
+        await _DbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateAssignmentById(Assignment assignment)
+    {
+        var assignmentToChange = await GetAssignmentById(assignment.Id);
+
+        assignmentToChange.Name = assignment.Name;
+        assignmentToChange.Description = assignment.Description;
+        assignmentToChange.Status = assignment.Status;
+
+        await _DbContext.SaveChangesAsync();
+    }
+}
---
+++ DLL/Repository/IAssignmentRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using DAL.Entities;
+using DAL.Enums;
+
+namespace DAL.Repository;
+
+public interface IAssignmentRepository
+{
+    Task<Assignment?> GetAssignmentById(int id);
+    Task<IEnumerable<Assignment>> GetAssignmentsByStatus(EnumStatus status);
+    Task DeleteAssignment(int id);
+    Task UpdateAssignmentById(Assignment assignment);
+    Task PutAssignment(Assignment assignment);
+}
